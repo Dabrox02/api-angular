edfcc9c471e06dd13ff60d7bf71762bb61bc68a0
Jaider Mendoza <jaiderstyvenmendoza@gmail.com>
2024-04-11 17:28:06 -0500

    feat: :sparkles: add carousel component with pexels api

--- src/app/paginas/inicio/inicio.component.ts
+++ src/app/paginas/inicio/inicio.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PexelApiService } from '../../servicios/pexel-api.service';
 
 @Component({
   selector: 'app-inicio',
@@ -7,6 +8,29 @@ import { Component } from '@angular/core';
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
-export class InicioComponent {
+export class InicioComponent implements OnInit{
+  public imagenes: any[] = [];
+
+  // Este constructor de la clase inyecta el servicio PexelApiService y lo almacena en una propiedad privada llamada pexelApiService 
+  constructor(private pexelApiService: PexelApiService) { }
+
+  ngOnInit(): void {
+    this.getImagenes("people");
+  }
+
+  getImagenes(query: string):void {
+    this.pexelApiService.getData(query)  // Se llama al método getData del servicio PexelApiService
+      .subscribe({  // El método getData devuelve un Observable, por lo que se utiliza subscribe para manejar la respuesta
+        next: (res) => { 
+          // Se actualizan las imagenes almacenando la respuesta (res) en la propiedad imagenes
+          this.imagenes = res.photos;
+          console.log(this.imagenes);
+        },
+        error: (err) => {  
+          // La función error se ejecuta si ocurre un error al obtener los datos (err)
+          console.log(err);
+        }
+      });
+  }
 
 }
---
+++ src/app/servicios/pexel-api.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class PexelApiService { 
+
+  API_URL: string = 'https://api.pexels.com/v1/search?query=';  
+  // Esta línea declara una constante pública de cadena API_URL que contiene la URL base para las llamadas a la API
+
+  constructor(private http: HttpClient) { }  
+  // Este constructor de la clase se inyecta el servicio HttpClient y lo almacena en una propiedad privada llamada http
+
+  getData(query: string): Observable<any> {  // Esta línea define un método llamado getData que devuelve un Observable de cualquier tipo
+    let headers = new HttpHeaders();
+    headers = headers.set('Authorization', 'taB7YcLoLPEyhlEBq8NzYPMgKeTVSciHkZr5RsMYJWBSFeijH8vBYuWE'); // Agregamos cabecero de autorizacion
+    headers = headers.set('Access-Control-Allow-Origin', '*');
+
+    return this.http.get<any>(`${this.API_URL}${query}&per_page=5`, {headers}).pipe(res => res);  
+    // Esta línea devuelve un Observable creado por el método HttpClient.get. Obtiene datos de la API_URL y usa el operador pipe para simplemente devolver la respuesta (res) sin modificaciones.
+  }
+}
