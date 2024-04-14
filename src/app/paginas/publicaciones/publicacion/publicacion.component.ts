2be32433232d0f63d34d24cd276dcd6defe4525c
Jaider Mendoza <jaiderstyvenmendoza@gmail.com>
2024-04-14 15:08:58 -0500

    feat: :sparkles: search engine for publications

---
+++ src/app/paginas/publicaciones/publicacion/publicacion.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Publicacion } from '../../../interfaces/publicacion.interface';
+import { Usuario } from '../../../interfaces/usuario';
+import { ConsumoApiService } from '../../../servicios/consumo-api.service';
+
+@Component({
+  selector: 'app-publicacion',
+  standalone: true,
+  imports: [],
+  templateUrl: './publicacion.component.html',
+  styleUrl: './publicacion.component.css'
+})
+export class PublicacionComponent {
+  @Input() publicacion!: Publicacion;
+  @Input() usuario!: Usuario | undefined;
+
+}
--- src/app/paginas/publicaciones/publicaciones.component.ts
+++ src/app/paginas/publicaciones/publicaciones.component.ts
@@ -1,39 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ConsumoApiService } from '../../servicios/consumo-api.service';
 import { Publicacion } from '../../interfaces/publicacion.interface';
 import { Usuario } from '../../interfaces/usuario';
+import { PublicacionComponent } from "./publicacion/publicacion.component";
 
 @Component({
   selector: 'app-publicaciones',
   standalone: true,
-  imports: [],
   templateUrl: './publicaciones.component.html',
-  styleUrl: './publicaciones.component.css'
+  styleUrl: './publicaciones.component.css',
+  imports: [PublicacionComponent]
 })
 export class PublicacionesComponent implements OnInit {  // Implementacion de interfaz OnInit
 
   // Esta línea declara una propiedad pública llamada publicaciones que es un arreglo vacío de tipo Publicacion
   public publicaciones: Publicacion[] = [];
   public usuarios: Usuario[] = [];
+  textoBuscado: string = "";
 
   // Este constructor de la clase inyecta el servicio ConsumoApiService y lo almacena en una propiedad privada llamada consumoApiService
-  constructor(private consumoApiService: ConsumoApiService) { }  
-  
+  constructor(private consumoApiService: ConsumoApiService) { }
+
   // Este es un método obligatorio de la interfaz OnInit que se llama automáticamente cuando se inicializa el componente
-  ngOnInit(): void {  
+  ngOnInit(): void {
     // Dentro de ngOnInit se llama al método getPublicaciones para obtener los datos al iniciar el componente
-    this.getPublicaciones();  
+    this.getPublicaciones();
     this.getUsuarios();
   }
 
-  getPublicaciones():void {
+  getPublicaciones(): void {
     this.consumoApiService.getData("posts")  // Se llama al método getData del servicio ConsumoApiService
       .subscribe({  // El método getData devuelve un Observable, por lo que se utiliza subscribe para manejar la respuesta
-        next: (res) => { 
+        next: (res) => {
           // Se actualizan las publicaciones almacenando la respuesta (res) en la propiedad publicaciones
-          this.publicaciones = res;  
+          this.publicaciones = res;
         },
-        error: (err) => {  
+        error: (err) => {
           // La función error se ejecuta si ocurre un error al obtener los datos (err)
           console.log(err);
         }
@@ -43,11 +45,11 @@ export class PublicacionesComponent implements OnInit {  // Implementacion de in
   getUsuarios(): void {
     this.consumoApiService.getData(`users`)  // Se llama al método getData del servicio ConsumoApiService
       .subscribe({  // El método getData devuelve un Observable, por lo que se utiliza subscribe para manejar la respuesta
-        next: (res) => { 
+        next: (res) => {
           // Se actualizan los usuarios almacenando la respuesta (res) en la propiedad publicaciones
-          this.usuarios = res;  
+          this.usuarios = res;
         },
-        error: (err) => {  
+        error: (err) => {
           // La función error se ejecuta si ocurre un error al obtener los datos (err)
           console.log(err);
         }
@@ -57,4 +59,10 @@ export class PublicacionesComponent implements OnInit {  // Implementacion de in
   getUsuarioPorId(userId: number): Usuario | undefined {
     return this.usuarios.find((user) => user.id === userId);
   }
+
+  buscarTexto(input: HTMLInputElement) {
+    this.textoBuscado = input.value.toLowerCase();
+    console.log(this.textoBuscado);
+  }
+
 }
